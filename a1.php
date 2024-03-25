f77a7ffbf003c6cdf54b389b538a738a912236ae
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-03-26 03:42:49 +0530

    Add files via upload

---
+++ a1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo"Nayani";?></title>
+</head>
+<body>
+    <h1><?php echo"Nayani";?></h1>
+    <p><?php echo"Nayani";?></p>
+    <a href="#"><?php echo"Nayani";?></a>
+
+  
+</body>
+</html>
