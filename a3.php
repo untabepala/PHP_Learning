bb78c7a925c1e52e54c681f70771d7e94af0108f
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-05-03 07:40:41 +0530

    Add files via upload

---
+++ a3.php
@@ -0,0 +1,14 @@
+<?php
+$variable="Academy";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title><?php echo $variable;?></title>
+</head>
+<body>
+    <h1><?php echo $variable;?></h1>
+    <a href="#"><?php echo $variable;?></a>
+</body>
+</html>
