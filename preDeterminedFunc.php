44caf094ca71ca87cb0f6cb50d40602fbdd347e1
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-05-09 05:26:56 +0530

    Add files via upload

---
+++ preDeterminedFunc.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo strlen("Nayani")."<br>";
+    echo str_word_count("cha la na ")."<br>";
+    echo strrev("string")."<br>";
+    echo strpos("Nayani is good girl","is");
+
+    echo sin(90)."<br>";
+    echo cos(90)."<br>";
+    echo log10(100)."<br>";
+    ?>
+</body>
+</html>
