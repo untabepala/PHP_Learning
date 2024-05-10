1f485ccaeb02c82d5e49f526033d801c78005774
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-05-10 07:45:36 +0530

    Add files via upload

---
+++ dataTypes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   echo $myString="DTK's TV<br>";
+   echo $myString='DTK';
+
+   echo $mynum=10;
+
+   $mynum=Null;
+   echo $mynum;
+   //$myBool
+   $myArray=array('DTK','TV','Holdings');
+   echo $myArray['1'];
+   ?> 
+</body>
+</html>
