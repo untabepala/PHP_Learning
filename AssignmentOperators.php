b2fbed0ba4ac320ece8a0b6a3518e75ac6b63536
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-05-12 05:25:03 +0530

    Add files via upload

---
+++ AssignmentOperators.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x=5;
+    echo $x."<br>";
+
+    $x*=15;
+    echo $x;
+    ?>
+</body>
+</html>
