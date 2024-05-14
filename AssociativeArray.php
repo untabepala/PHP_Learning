deb539b7b7849810d0f17bc4cd6fa6b203fdca63
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-05-15 02:32:39 +0530

    Add files via upload

---
+++ AssociativeArray.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $myarray=array('key1'=>10,'key2'=>15);
+    echo $myarray['key1'];
+echo"<br>";
+    $x=array("a"=>"apple","b"=>"ball");
+    $y=array("r"=>"red","g"=>"green");
+    print_r($x+$y);
+    ?>
+</body>
+</html>
