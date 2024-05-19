a3987987ace96065d35a940e8313ffb8c3fce023
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-05-19 08:27:26 +0530

    Add files via upload

---
+++ SwitchOperators.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $x=25;
+    switch($x){
+        case 5:
+            echo "value is 5";
+            break;
+        case 10:
+            echo "value is 10";
+            break;
+        case 25:
+                echo "value is 25";
+                break;
+        default:
+        echo"more than 25";
+        break;
+    }
+    ?>
+</body>
+</html>
