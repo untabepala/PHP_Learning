de629e5602aeabee736ab82854f8d7c771ea1ee2
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-05-22 07:19:45 +0530

    Add files via upload

---
+++ UserDefinedFunc.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*function FuncName($x){
+        $sq=$x*$x;
+        echo "Squre of the number is: ".$sq;
+    }
+    FuncName(5);*/
+    //include 'new.php';   //mey file ekk nttn warning witry krnne
+
+    require 'new.php';  //meya file ek nttn error ekk pennw
+    ?>
+</body>
+</html>
