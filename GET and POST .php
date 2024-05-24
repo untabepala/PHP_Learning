1e40b7d83f0473bea4ec04544108c3e7a9ac461f
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-05-24 08:56:20 +0530

    Add files via upload

---
+++ GET and POST .php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    ?>
+
+    <form action="GET">
+        <input type="hidden" name="name" value="DTK">
+        <input type="text" name="userInput">
+        <input type="submit" name="submit" value="Click Here">
+
+    </form>
+</body>
+</html>
